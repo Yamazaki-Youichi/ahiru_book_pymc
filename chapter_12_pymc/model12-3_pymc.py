13ba75ac5e313262d369cc0b2ad3f14a329ff58c
mathetake <cz.rk.t0415y.g@gmail.com>
2017-01-21 15:54:14 +0900

    model12-3

--- chapter_12_pymc/model12-3_pymc.py
+++ chapter_12_pymc/model12-3_pymc.py
@@ -19,38 +19,24 @@ n_times = len(df["X"].unique())
 
 basic_model = Model()
 
-#GaussianRandomWalkを使う方法と使わない方法どちらも実装しました。
 
 #GaussianRandomWalkを使わない方法
 with basic_model:
     #事前分布
     s_mu = HalfNormal('s_mu', sd=100) #隣接時刻の状態の誤差
     s_Y =  HalfNormal('s_Y', sd=100) #各時刻における状態と観測の誤差
-    mu_0 = Normal('mu_0',mu=0, sd=100) #初期状態
+    mu_0 = Normal('mu_0',mu=0, sd=100) #t=0初期状態
+    mu_1 = Normal('mu_1',mu=0, sd=100) #t=1初期状態
     
     #誤差項
-    e_mu = Normal('e_mu', mu=0, sd=s_mu, shape =n_times-1)
+    e_mu = Normal('e_mu', mu=0, sd=s_mu, shape =n_times-2)
     
     mu = tt.zeros((n_times))
     mu = tt.set_subtensor(mu[0], mu_0)
-    for i in list(range(n_times-1)):
-        mu = tt.set_subtensor(mu[i+1], mu[i]+e_mu[i])
-
+    mu = tt.set_subtensor(mu[1], mu_1)
+    for i in list(range(n_times-2)):
+        mu = tt.set_subtensor(mu[i+2], 2*mu[i+1] - mu[i] + e_mu[i])
     #likelihood
-    Y_obs = Normal('Y_obs', mu=mu, sd=s_Y, observed=Y)
-
-    #サンプリング
-    trace = sample(1000)
-    summary(trace)
-    
-#GaussianRandomWalkを使う方法
-with basic_model:
-    #事前分布
-    s_mu = HalfNormal('s_mu', sd=100) #隣接時刻の状態の誤差
-    s_Y =  HalfNormal('s_Y', sd=100) #各時刻における状態と観測の誤差
-
-    #likelihood
-    mu = GaussianRandomWalk("mu",s_mu, shape=n_times )    
     Y_obs = Normal('Y_obs', mu=mu, sd=s_Y, observed=Y)
 
     #サンプリング
