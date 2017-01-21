0d2cba9ad6fa01fee826e4d489097d0d4742b497
mathetake <cz.rk.t0415y.g@gmail.com>
2017-01-21 15:16:54 +0900

    model12-1/2

---
+++ chapter_12_pymc/model12-1_pymc.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pymc3 as pm
+from pymc3 import Model, Normal, GaussianRandomWalk, HalfNormal
+from pymc3 import NUTS, sample
+from scipy import optimize
+from pymc3 import traceplot
+from pymc3 import summary
+import theano.tensor as tt
+import theano as T
+
+df = pd.read_csv("../data/data-ss1.txt")
+T = df.values[:,0].astype(np.float32)
+Y = df.values[:,1].astype(np.float32)
+
+n_times = len(df["X"].unique())
+
+#shape = クラスの数の確率変数に、クラスの値を取るデータ数次元のベクトルを入れる操作がありますが
+#その詳細な説明は(https://pymc-devs.github.io/pymc3/notebooks/GLM-hierarchical.html)参照
+
+basic_model = Model()
+
+#GaussianRandomWalkを使う方法と使わない方法どちらも実装しました。
+
+#GaussianRandomWalkを使わない方法
+with basic_model:
+    #事前分布
+    s_mu = HalfNormal('s_mu', sd=100) #隣接時刻の状態の誤差
+    s_Y =  HalfNormal('s_Y', sd=100) #各時刻における状態と観測の誤差
+    mu_0 = Normal('mu_0',mu=0, sd=100) #初期状態
+    
+    #誤差項
+    e_mu = Normal('e_mu', mu=0, sd=s_mu, shape =n_times-1)
+    
+    mu = tt.zeros((n_times))
+    mu = tt.set_subtensor(mu[0], mu_0)
+    for i in list(range(n_times-1)):
+        mu = tt.set_subtensor(mu[i+1], mu[i]+e_mu[i])
+
+    #likelihood
+    Y_obs = Normal('Y_obs', mu=mu, sd=s_Y, observed=Y)
+
+    #サンプリング
+    trace = sample(1000)
+    summary(trace)
+    
+#GaussianRandomWalkを使う方法
+with basic_model:
+    #事前分布
+    s_mu = HalfNormal('s_mu', sd=100) #隣接時刻の状態の誤差
+    s_Y =  HalfNormal('s_Y', sd=100) #各時刻における状態と観測の誤差
+
+    #likelihood
+    mu = GaussianRandomWalk("mu",s_mu, shape=n_times )    
+    Y_obs = Normal('Y_obs', mu=mu, sd=s_Y, observed=Y)
+
+    #サンプリング
+    trace = sample(1000)
+    summary(trace)
